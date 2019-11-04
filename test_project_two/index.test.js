7bce1706db872db498aca8318b47224194fa510a
Lehman Class <davrovana@hotmail.com>
2019-11-04 00:55:40 +0000

    touching up test file

--- test_project_two/index.test.js
+++ test_project_two/index.test.js
@@ -1,41 +1,4 @@
-/*
-
-Test One:
-
-curl 'localhost:3000/test_one?fruit=dragonfruit&cake=genoise' should return -> { "message": { "fruit": "dragonfruit", "cake": "genoise" } }
-
-Test Two:
-
-curl -d '{ "fruit": "kiwi", "cake": "sponge" }' -H 'Content-Type: application/json' 'localhost:3000/test_two' should return -> { "message": "i love to eat kiwi with sponge" }
-
-Test Three:
-
-curl -H 'Authorization: Bearer projecttwo' 'localhost:3000/test_three/grape/angelfood' should return -> { "message": "you sent grape and angelfood, but I only eat angelfood!" }
-
-curl -H 'Authorization:: Bearer something else' 'localhost:3000/test_three/grape/angelfood' should return -> { "message": "unauthorized"}
-
-Test Four:
-
-curl -s -d 'fruit=kiwi&cake=sponge' -H 'Content-Type: application/x-www-form-urlencoded' 'localhost:3000/test_four' should return -> { "message": i am getting really sick of eating kiwi after filling up on sponge" }
-
-Test Five:
-
-curl -X PUT -d '{ "fruit": "orange", "cake": "bad" }' -H 'Content-Type: application/json' 'localhost:3000/test_five/write' should return -> { "message": "you sent orange and bad" }
-
-curl localhost:3000/test_five/read should return -> { "orange": 1, "bad": 1 }
-
-curl -X PUT -d '{ "fruit": "cherry", "cake": "bad" }' -H 'Content-Type: application/json' 'localhost:3000/test_five/write' should return -> { "message": "you sent cherry and bad" }
-
-curl localhost:3000/test_five/read should return -> { "orange":1, "bad":2, "cherry": 1 }
-
-*/
-
 const request = require('request-promise-native');
-// const { exec } = require('child_process');
-
-// const startServer = () => {
-
-// };
 
 const LOCALHOST = 'http://localhost:3000';
 const UNAUTHORIZED = '{"message":"unauthorized"}';
